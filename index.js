bb939b71b06e2ccad19e0ba62a391014760fc423
Hanif Carroll <hanif.carroll@8base.com>
2018-02-26 16:49:23 -0500

    Added heroku integration

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("App started");
+});
