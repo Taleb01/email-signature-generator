9f10e6a39d285dd9e699a7150e6e66faa21dca7d
Hanif Carroll <hanif.carroll@8base.com>
2018-02-28 15:28:49 -0500

    Changed styling

--- client/src/features/signature-generator/SignatureResult.js
+++ client/src/features/signature-generator/SignatureResult.js
@@ -21,14 +21,14 @@ export class SignatureResult extends Component {
       <img
         src="https://ci3.googleusercontent.com/proxy/nWPfVeCBuLYU6LCPNUSsqL8VXX2gnEpH9qtxlq4dKVLcS79UKd9qB3L7AwgrcllGvw1NzDXjDsrONO9Dagc2Cjxex_en3ATZC7dYFzyVMMmB9d8RtYjJTgJY7VKTAqTyhOQSi0mwqOri37BywgkRdGr7w8RsnAqqqZ3mdvjj4hTLuUItefkYOoOsX1MHysaGu9HgxdGnS0xXivpZpGdJrw=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1ADpXElsi-DoEbeuD3td_rn1y3QRazC1S&amp;revid=0BxKTdJnCbLzfVDhxQk01dktIa2tDQVc1SDNURzhGK2V3NDBVPQ"
         alt="Twitter"
-        style="padding-right:5px"
-        width="14"
+        style="padding-right:0px"
+        width="9"
         class="CToWUd"
       />
       &nbsp;
       <a
         href="https://twitter.com/${twitter}"
-        style="color:rgb(52,63,79);text-decoration:none"
+        style="color:rgb(52,63,79);letter-spacing:.5px;text-decoration:none"
         target="_blank"
       >
         @${twitter}
@@ -49,14 +49,14 @@ export class SignatureResult extends Component {
         <img
           src="https://ci6.googleusercontent.com/proxy/ejo72VgOXJBs3smfFpN0WkiVXKZOcZWHSKpfcW3hJCs7mEVYlsySITBmMcn9fJhgPBht04XeUgB35qXbVyqNggaqB-M6Lp0G2CVqsYBAlRsnEYqHfjTtJqqhik58cA1r7-TasjATh0sAYdQJFiK50rsPGshVJbx-VlORTtpJZ_c8xmCrjf4a3fw1K-eIdjyJ1ftE0yAHDAQQphSezoui1Q=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1hO6I9DlNsCWGBTO-rLIHCiyVpxreEBmB&amp;revid=0BxKTdJnCbLzfcERzSE5aME9XQzJXZnpNUlBVckNBRGkrZlk0PQ"
           alt="LinkedIn"
-          style="padding-right:5px"
-          width="14"
+          style="padding-right:0px"
+          width="9"
           class="CToWUd"
         />
         &nbsp;
         <a
           href="https://www.linkedin.com/in/${linkedIn}"
-          style="display:inline-block;color:rgb(52,63,79);margin-right:10px;text-decoration:none"
+          style="display:inline-block;color:rgb(52,63,79);letter-spacing:.5px;margin-right:10px;text-decoration:none"
           target="_blank"
         >
           /in/${linkedIn}
@@ -80,24 +80,24 @@ export class SignatureResult extends Component {
               <img
                 src="https://ci6.googleusercontent.com/proxy/zNHPOd0e0QVnZarTZR-R1t0GKYkuY7BSx7FIYBboNZzZlAEwGH_qQv1_UcYiO11WcQse8a5Au0PN_ILo5yyHg6_J-j9QpIYChU2foxE7i6MS5Qt8UP3p9xa3JewFbi9OH_W8SxJj9To3_TdbNAjidKByIBu7FXGIIPjkR_LCJqzXiXRt0gC4poBFYyoFgzWkDGjSJyBtISnllLo0V2T__g=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1ByaU1RBqgVAzNrTJcqojlJ9rdkkVI7I1&amp;revid=0BxKTdJnCbLzfS2hoOVIvN2dQY3ExdmxzQzZRenNmWS9iYnFZPQ"
                 alt="logo"
-                width="81"
+                width="60"
                 class="CToWUd"
                 style="margin-left: 15px;"
               />
             </td>
-            <td style="width:30px" width="30">
+            <td style="width:14px" width="30">
               &nbsp;
             </td>
-            <td style="line-height:26px">
+            <td style="line-height:18px">
               <div style="height:30px">&nbsp;</div>
-              <div style="font-size:18px;font-weight:600">${name}</div>
-              <div style="text-transform:uppercase;font-size:14px">${title}</div>
-              <div style="font-size:14px">
+              <div style="font-size:13px;font-weight:600;letter-spacing:.5px;">${name}</div>
+              <div style="text-transform:uppercase;font-size:9px;letter-spacing:.75px">${title}</div>
+              <div style="font-size:10px">
                 <span style="color:rgb(233,66,53)">m.</span>
                 &nbsp;
                 <a
                   href="tel:${phone.replace(/[^0-9]/g, '')}"
-                  style="display:inline-block;color:rgb(52,63,79);margin-right:10px;text-decoration:none"
+                  style="display:inline-block;color:rgb(52,63,79);letter-spacing:.5px;margin-right:10px;text-decoration:none"
                   target="_blank"
                 >
                   ${
@@ -113,13 +113,13 @@ export class SignatureResult extends Component {
                 &nbsp;
                 <a
                   href="mailto:${email}"
-                  style="color:rgb(52,63,79);text-decoration:none"
+                  style="color:rgb(52,63,79);letter-spacing:.5px;text-decoration:none"
                   target="_blank"
                 >
                   ${email}
                 </a>
               </div>
-              <div style="font-size:14px">
+              <div style="font-size:10px">
                 ${this.renderLinkedIn()}
                 ${this.renderTwitter()}
               </div>
@@ -145,24 +145,24 @@ export class SignatureResult extends Component {
               <img
                 src="https://ci6.googleusercontent.com/proxy/zNHPOd0e0QVnZarTZR-R1t0GKYkuY7BSx7FIYBboNZzZlAEwGH_qQv1_UcYiO11WcQse8a5Au0PN_ILo5yyHg6_J-j9QpIYChU2foxE7i6MS5Qt8UP3p9xa3JewFbi9OH_W8SxJj9To3_TdbNAjidKByIBu7FXGIIPjkR_LCJqzXiXRt0gC4poBFYyoFgzWkDGjSJyBtISnllLo0V2T__g=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1ByaU1RBqgVAzNrTJcqojlJ9rdkkVI7I1&amp;revid=0BxKTdJnCbLzfS2hoOVIvN2dQY3ExdmxzQzZRenNmWS9iYnFZPQ"
                 alt="logo"
-                width="81"
+                width="60"
                 class="CToWUd"
                 style="margin-left: 15px;"
               />
             </td>
-            <td style="width:30px" width="30">
+            <td style="width:14px" width="30">
               &nbsp;
             </td>
-            <td style="line-height:26px">
-              <div style="height:30px">&nbsp;</div>
-              <div style="font-size:18px;font-weight:600">${name}</div>
-              <div style="text-transform:uppercase;font-size:14px">${title}</div>
-              <div style="font-size:14px">
+            <td style="line-height:18px">
+              <div style="height:28px">&nbsp;</div>
+              <div style="font-size:13px;font-weight:600;letter-spacing:.5px;">${name}</div>
+              <div style="text-transform:uppercase;font-size:9px;letter-spacing:.75px">${title}</div>
+              <div style="font-size:10px">
                 <span style="color:rgb(233,66,53)">m.</span>
                 &nbsp;
                 <a
                   href="tel:${phone.replace(/[^0-9]/g, '')}"
-                  style="display:inline-block;color:rgb(52,63,79);margin-right:10px;text-decoration:none"
+                  style="display:inline-block;color:rgb(52,63,79);letter-spacing:.5px;margin-right:10px;text-decoration:none"
                   target="_blank"
                 >
                   ${
@@ -178,24 +178,24 @@ export class SignatureResult extends Component {
                 &nbsp;
                 <a
                   href="mailto:${email}"
-                  style="color:rgb(52,63,79);text-decoration:none"
+                  style="color:rgb(52,63,79);letter-spacing:.5px;text-decoration:none"
                   target="_blank"
                 >
                   ${email}
                 </a>
               </div>
-              <div style="font-size:14px">
+              <div style="font-size:10px">
                 <img
                   src="https://ci6.googleusercontent.com/proxy/ejo72VgOXJBs3smfFpN0WkiVXKZOcZWHSKpfcW3hJCs7mEVYlsySITBmMcn9fJhgPBht04XeUgB35qXbVyqNggaqB-M6Lp0G2CVqsYBAlRsnEYqHfjTtJqqhik58cA1r7-TasjATh0sAYdQJFiK50rsPGshVJbx-VlORTtpJZ_c8xmCrjf4a3fw1K-eIdjyJ1ftE0yAHDAQQphSezoui1Q=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1hO6I9DlNsCWGBTO-rLIHCiyVpxreEBmB&amp;revid=0BxKTdJnCbLzfcERzSE5aME9XQzJXZnpNUlBVckNBRGkrZlk0PQ"
                   alt="LinkedIn"
-                  style="padding-right:5px"
-                  width="14"
+                  style="padding-right:0px"
+                  width="9"
                   class="CToWUd"
                 />
                 &nbsp;
                 <a
                   href="https://www.linkedin.com/in/${linkedIn}"
-                  style="display:inline-block;color:rgb(52,63,79);margin-right:10px;text-decoration:none"
+                  style="display:inline-block;color:rgb(52,63,79);letter-spacing:.5px;margin-right:10px;text-decoration:none"
                   target="_blank"
                 >
                   /in/${linkedIn}
@@ -203,14 +203,14 @@ export class SignatureResult extends Component {
                 <img
                   src="https://ci3.googleusercontent.com/proxy/nWPfVeCBuLYU6LCPNUSsqL8VXX2gnEpH9qtxlq4dKVLcS79UKd9qB3L7AwgrcllGvw1NzDXjDsrONO9Dagc2Cjxex_en3ATZC7dYFzyVMMmB9d8RtYjJTgJY7VKTAqTyhOQSi0mwqOri37BywgkRdGr7w8RsnAqqqZ3mdvjj4hTLuUItefkYOoOsX1MHysaGu9HgxdGnS0xXivpZpGdJrw=s0-d-e1-ft#https://docs.google.com/uc?export=download&amp;id=1ADpXElsi-DoEbeuD3td_rn1y3QRazC1S&amp;revid=0BxKTdJnCbLzfVDhxQk01dktIa2tDQVc1SDNURzhGK2V3NDBVPQ"
                   alt="Twitter"
-                  style="padding-right:5px"
-                  width="14"
+                  style="padding-right:0px"
+                  width="9"
                   class="CToWUd"
                 />
                 &nbsp;
                 <a
                   href="https://twitter.com/${twitter}"
-                  style="color:rgb(52,63,79);text-decoration:none"
+                  style="color:rgb(52,63,79);letter-spacing:.5px;text-decoration:none"
                   target="_blank"
                 >
                   @${twitter}
